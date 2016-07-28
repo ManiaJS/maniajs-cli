1ddf87c1e0b751e2df7c5be95b97b45e697be239
Tom Valk <tomvalk@gmail.com>
2016-07-28 14:42:26 +0200

    Change starter to support new structure

--- template/maniajs.js
+++ template/maniajs.js
@@ -6,7 +6,8 @@
  */
 process.env.CONFIG_LOCATION = __dirname + '/config.yaml';
 try {
-  require('maniajs'); // Will start ManiaJS.
+  let ManiaJS = require('maniajs'); // Will start ManiaJS.
+  if (ManiaJS.hasOwnProperty('Start')) ManiaJS.Start();
 } catch (err) {
   console.log('Please execute \'npm install\' to install all the dependencies, or with the CLI tool: \'maniajs update\'!');
   process.exit(1);
